87ee82ebe56934c19f161a6ec65ba4c71f3f1fcc
jacogrinwis <jacogrinwis@gmail.com>
2017-10-02 13:16:58 +0200

    Initial Commit

---
+++ Database/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Database
+//
+//  Created by Jaco Grinwis on 02-10-17.
+//  Copyright © 2017 Jaco Grinwis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Database/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Database
+//
+//  Created by Jaco Grinwis on 02-10-17.
+//  Copyright © 2017 Jaco Grinwis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
