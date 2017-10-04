773f064011a1f8af4aa7e79087b85a5df5960a37
Jaco Grinwis <jacogrinwis@gmail.com>
2017-10-04 18:10:36 +0200

    Cleaning code up by setting up extensions

--- Database/ViewController.swift
+++ Database/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Database
 //
-//  Created by Jaco Grinwis on 02-10-17.
+//  Created by Jaco Grinwis on 04-10-17.
 //  Copyright © 2017 Jaco Grinwis. All rights reserved.
 //
 
@@ -15,87 +15,93 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        /*
-        let product1Id = ProductEntity.shared.insert(name: "Nikon D800",
-                                                     description: "FX Camera",
-                                                     price: 4000.00,
-                                                     purchasedAt: nil,
-                                                     store: "KameraExpress",
-                                                     storeUrl: "http://www.kamera-express.nl/",
-                                                     productUrl: "http://www.kamera-express.nl/nikon-d800/",
-                                                     thumbnail: "")
         
-        let product2Id = ProductEntity.shared.insert(name: "Nikon D300S",
-                                                     description: "DX Camera",
-                                                     price: 1400.60,
-                                                     purchasedAt: nil,
-                                                     store: "KameraExpress",
-                                                     storeUrl: "http://www.kamera-express.nl/",
-                                                     productUrl: "http://www.kamera-express.nl/nikon-d300s/",
-                                                     thumbnail: "")
+        // databaseInjection()
         
-        let product3Id = ProductEntity.shared.insert(name: "Nikon D3300",
-                                                     description: "DX Camera",
-                                                     price: 400.95,
-                                                     purchasedAt: nil,
-                                                     store: "CameraNU",
-                                                     storeUrl: "http://www.cameranu/",
-                                                     productUrl: "http://www.cameranu.nl/nikon-d3300/",
-                                                     thumbnail: "")
-        */
-        
-        //print(ProductEntity.shared.getAll())
-        
-        /*
-        if let productQuery: AnySequence<Row> = ProductEntity.shared.queryAll() {
-            var productArray = [Any]()
+        if let productQuery:AnySequence<Row> = ProductEntity.shared.queryAll() {
             for eachProduct in productQuery {
                 ProductEntity.shared.toString(product: eachProduct)
-                //productArray.append(name: "")
-                let newArray = Array(productQuery)
-                print(newArray)
             }
         }
-        */
-        
-        /*
-        if let productQuery: AnySequence<Row> = ProductEntity.shared.queryAll() {
-            let newArray = Array(productQuery)
-            print(newArray)
-        }
-        */
- 
-        // AnySequence<Row>
-        print(ProductEntity.shared.count()!)
-        let obj = ProductEntity.shared.queryAll()
-        print(obj!)
     }
     
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    // MARK: - Database Injection
+    func databaseInjection() {
+        let product1Id = ProductEntity.shared.insert(
+            name: "Nikon D800",
+            description: "FX Camera",
+            price: 4000.00,
+            purchasedAt: nil,
+            store: "KameraExpress",
+            storeUrl: "http://www.kamera-express.nl/",
+            productUrl: "http://www.kamera-express.nl/nikon-d800/",
+            thumbnail: ""
+        )
+        print(product1Id!)
+        
+        let product2Id = ProductEntity.shared.insert(
+            name: "Nikon D300S",
+            description: "DX Camera",
+            price: 1400.60,
+            purchasedAt: nil,
+            store: "KameraExpress",
+            storeUrl: "http://www.kamera-express.nl/",
+            productUrl: "http://www.kamera-express.nl/nikon-d300s/",
+            thumbnail: ""
+        )
+        print(product2Id!)
+        
+        let product3Id = ProductEntity.shared.insert(
+            name: "Nikon D3300",
+            description: "DX Camera",
+            price: 400.95,
+            purchasedAt: nil,
+            store: "CameraNU",
+            storeUrl: "http://www.cameranu/",
+            productUrl: "http://www.cameranu.nl/nikon-d3300/",
+            thumbnail: ""
+        )
+        print(product3Id!)
+    }
+}
+
+// MARK: - TableView
+extension ViewController: NSTableViewDataSource {
     func numberOfRows(in tableView: NSTableView) -> Int {
         return ProductEntity.shared.count()!
     }
     
     func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
-        return 20
+        return 17
     }
-    
-    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        guard let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "TableCell"), owner: self) as? TableCellView else { return nil }
-        cell.idLabel.stringValue = ""
-        cell.nameLabel.stringValue = ""
-        cell.descriptionLabel.stringValue = ""
-        cell.priceLabel.stringValue = ""
-        cell.purchasedAtLabel.stringValue = ""
-        cell.storeLabel.stringValue = ""
-        cell.storeUrlLabel.stringValue = ""
-        cell.productUrlLabel.stringValue = ""
-        cell.thumbnailLabel.stringValue = ""
-        cell.createdAtLabel.stringValue = ""
-        cell.updatedAtLabel.stringValue = ""
-        return cell
-    }
-
-
 }
 
+extension ViewController: NSTableViewDelegate {
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "TableCell"), owner: self) as? TableCellView {
+            cell.idLabel.stringValue = "test"           // <- How do I get the contents from the database here?
+            cell.nameLabel.stringValue = "test"         // <- And here
+            cell.descriptionLabel.stringValue = "test"  // <- And here
+            cell.priceLabel.stringValue = "test"        // <- And here
+            cell.purchasedAtLabel.stringValue = "test"  // <- And here
+            cell.storeLabel.stringValue = "test"        // <- And here
+            cell.storeUrlLabel.stringValue = "test"     // <- And here
+            cell.productUrlLabel.stringValue = "test"   // <- And here
+            cell.thumbnailLabel.stringValue = "test"    // <- And here
+            cell.createdAtLabel.stringValue = "test"    // <- And here
+            cell.updatedAtLabel.stringValue = "test"    // <- And here
+            return cell
+        }
+        return nil
+    }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        print(tableView.selectedRow)
+    }
+}
